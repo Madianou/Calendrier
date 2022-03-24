49ed17d10a37e995032fbc2f7d68800bedd86d9e
Maxime Jougla <jouglamaxime2003@gmail.com>
2022-03-24 11:55:34 +0100

    Ajout du test de compare to // ne fonctionne pas

--- src/main/java/TestUnit/DateTest.java
+++ src/main/java/TestUnit/DateTest.java
@@ -15,4 +15,14 @@ class DateTest {
         assertEquals(28,Date.dernierJourDuMois(2,2022));// Test pour le mois de février qui compte 28 jours en 2022 car l'année n'est pas bissextile
         assertEquals(29,Date.dernierJourDuMois(29,2020));// Test pour le mois de février qui compte 29 jours en 2020 car l'année est bissextile
     }
+    @org.junit.jupiter.api.Test
+    @DisplayName("Test de compareTo")
+    void compareTo() {
+        Date date1 = new Date(1,1,2022);
+        Date date2 = new Date(31,12,2022);
+        Date date3 = new Date(1,1,2023);
+        assertEquals(0,date1.compareTo(date1));
+        assertEquals(-1,date1.compareTo(date2));
+        assertEquals(1,date3.compareTo(date2));
+    }
 }
